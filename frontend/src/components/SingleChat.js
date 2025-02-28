d30fe6c97d2a1ce65de96aaf2223a6dbae0b26cb
Palak Mishra <mishrapalak1804@gmail.com>
2025-02-28 11:41:42 +0530

    Chat Message fetch backend

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -1,16 +1,87 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ChatState } from '../Context/ChatProvider';
 import { Box, Text } from '@chakra-ui/layout';
-import { IconButton } from '@chakra-ui/react';
+import { FormControl, IconButton, Input, Spinner, useToast } from '@chakra-ui/react';
 import { ArrowBackIcon } from '@chakra-ui/icons';
 import { getSender, getSenderFull } from "../config/ChatLogics";
 import ProfileModal from './miscellaneous/ProfileModal';
 import UpdateGroupChatModal from './miscellaneous/UpdateGroupChatModal';
+import axios from 'axios';
+import './styles.css';
 
 const SingleChat = ({ fetchAgain, setFetchAgain}) => {
+    const [messages, setMessages]= useState([])
+    const [loading, setLoading] = useState(false);
+    const [newMessage, setNewMessage] = useState("");
 
+    const toast = useToast();
     const { user, selectedChat, setSelectedChat} = ChatState();
 
+    const fetchMessages = async () => {
+    if (!selectedChat) return;
+
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      setLoading(true);
+
+      const { data } = await axios.get(
+        `/api/message/${selectedChat._id}`,
+        config
+      );
+      console.log(messages);
+      
+      setMessages(data);
+      setLoading(false);
+
+    //   socket.emit("join chat", selectedChat._id);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to Load the Messages",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+    }
+  };
+
+    useEffect(()=>{
+        fetchMessages();
+    },[selectedChat]);
+    const sendMessage=async(event)=>{
+        if(event.key==="Enter" && newMessage){
+            try{
+                const config ={
+                    headers:{
+                        "Content-type": "application/json",
+                        Authorization: `Bearer ${user.token}`,
+                    },
+                };
+                
+                setNewMessage("");
+                const {data}= await axios.post('/api/message',{
+                    content: newMessage,
+                    chatId: selectedChat._id,
+                },
+                config
+            );
+
+            console.log(data);
+
+            
+            setMessages([...messages,data]);
+            }catch(error){}
+        }
+    };
+    const typingHandler=(e)=>{
+        setNewMessage(e.target.value);
+    };
   return (<>{
     selectedChat ? (
         <>
@@ -54,7 +125,27 @@ const SingleChat = ({ fetchAgain, setFetchAgain}) => {
          borderRadius="lg"
          overflowY="hidden"
         >
-            {/*Messages Here*/ }
+            {loading?(
+                <Spinner
+                    size="xl"
+                    w={20}
+                    h={20}
+                    alignSelf="center"
+                    margin="auto"
+                />
+            ):(
+                <div>
+                    {/* Messages */}
+                </div>
+            )}
+            <FormControl onKeyDown={sendMessage} isRequired mt={3}> 
+                <Input
+                variant="filled"
+                bg="#E0E0E0"
+                placeholder='Enter a message..'
+                onChange={typingHandler}
+                value={newMessage}
+                /> </FormControl>
         </Box>
         </>
     ) : (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,12 +8,14 @@ import ChatProvider from './Context/ChatProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider>
-    <BrowserRouter>
+  
+  <BrowserRouter>
+    <ChakraProvider>
       <ChatProvider>
         <App />
       </ChatProvider>
-    </BrowserRouter>
-  </ChakraProvider>
+    </ChakraProvider>
+  </BrowserRouter>,
+  
 );
 
